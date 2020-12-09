95cf2675353d391e63a2e670cb5d4cafa387d016
Panchis7u7 <smadrigal.rod@gmail.com>
2020-12-09 01:39:17 -0600

    Creacion de formulario simple usando bootstrap y react.

--- formularios_mysql/cliente/src/App.js
+++ formularios_mysql/cliente/src/App.js
@@ -1,11 +1,78 @@
+import React from "react";
+import {Container,Row,Form, FormGroup, FormControl, FormLabel, Button, Alert} from "react-bootstrap";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hello world!
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      nombre: "",
+      apellido: "",
+      alerta: false,
+      msgAlerta: "",
+      tipoAlerta: "satisfactoria",
+    };
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  //Añadir un registro.
+  addRegistro = () => {
+    var cabezales = new Headers();
+    cabezales.append("Content-Type", "application/json");
+    var cuerpo = JSON.stringify({nombre: this.state.nombre, apellido: this.state.apellido})
+    fetch("http://localhost:3001/api/create", {
+      method: "POST",
+      headers: cabezales,
+      body: cuerpo
+    }).then((respuesta) => respuesta.json())
+    .then((resultado) => {
+      console.log(resultado);
+      this.setState({
+        nombre: "",
+        apellido: "",
+        alerta: true,
+        msgAlerta: resultado.respuesta,
+        tipoAlerta: "satisfactoria",
+      });
+    });
+  };
+
+  render(){
+    return (
+    <div>
+      <Container>
+        {
+          this.state.alerta === true ? (
+          <Alert variant={this.state.tipoAlerta} onClose={() => {
+            this.setState({
+              alerta: false,
+            })
+          }} dismissible>
+            <Alert.Heading>{this.state.msgAlerta}</Alert.Heading>
+          </Alert>
+          ): null}
+        <Row>
+          <Form>
+            <FormGroup>
+              <FormLabel>Ingrese el nombre:</FormLabel>
+              <FormControl type="text" name="nombre" placeholder="Ingrese el nombre." onChange={this.handleChange} value={this.state.nombre}></FormControl>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Ingrese el Apellido</FormLabel>
+              <FormControl type="text" name="apellido" placeholder="Ingrese los apellidos." onChange={this.handleChange} value={this.state.apellido}/>
+            </FormGroup>
+            <Button onClick={this.addRegistro}>Guardar</Button>
+          </Form>
+        </Row>
+      </Container>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- formularios_mysql/servidor/index.js
+++ formularios_mysql/servidor/index.js
@@ -1,5 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+
+//Parse application/json
+app.use(bodyParser.json());
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "tienda_musica",
+});
+
+conn.connect((err) => {
+    if(err) throw err;
+    console.log("MYSQL conectado!");
+});
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -7,4 +25,8 @@ app.get("/", (req, res) => {
 
 app.listen(3001, () => {
     console.log("Corriendo en el puerto 3001");
+});
+
+app.post("/api/create", (req, res) => {
+    
 });
