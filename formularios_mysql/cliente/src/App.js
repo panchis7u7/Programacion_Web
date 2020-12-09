724a85affd3f3179231a41d5b521185ceaa16f69
Panchis7u7 <smadrigal.rod@gmail.com>
2020-12-09 11:09:17 -0600

    Insercion de registro a mysql de manera satisfactoria.

--- formularios_mysql/cliente/src/App.js
+++ formularios_mysql/cliente/src/App.js
@@ -6,8 +6,13 @@ class App extends React.Component{
   constructor(props){
     super(props)
     this.state={
+      cedula: "",
       nombre: "",
       apellido: "",
+      telefono: "",
+      telefono2: "",
+      direccion: "",
+      email: "",
       alerta: false,
       msgAlerta: "",
       tipoAlerta: "satisfactoria",
@@ -24,7 +29,15 @@ class App extends React.Component{
   addRegistro = () => {
     var cabezales = new Headers();
     cabezales.append("Content-Type", "application/json");
-    var cuerpo = JSON.stringify({nombre: this.state.nombre, apellido: this.state.apellido})
+    var cuerpo = JSON.stringify({
+      cedula: this.state.cedula,
+      nombre: this.state.nombre, 
+      apellido: this.state.apellido,
+      telefono: this.state.telefono,
+      telefono2: this.state.telefono2,
+      direccion: this.state.direccion,
+      email: this.state.email,
+    })
     fetch("http://localhost:3001/api/create", {
       method: "POST",
       headers: cabezales,
@@ -45,7 +58,7 @@ class App extends React.Component{
   render(){
     return (
     <div>
-      <Container>
+      <Container className="mh-auto">
         {
           this.state.alerta === true ? (
           <Alert variant={this.state.tipoAlerta} onClose={() => {
@@ -59,12 +72,32 @@ class App extends React.Component{
         <Row>
           <Form>
             <FormGroup>
-              <FormLabel>Ingrese el nombre:</FormLabel>
-              <FormControl type="text" name="nombre" placeholder="Ingrese el nombre." onChange={this.handleChange} value={this.state.nombre}></FormControl>
+              <FormLabel>Cedula</FormLabel>
+              <FormControl type="text" name="cedula" placeholder="Ingrese la direccion." onChange={this.handleChange} value={this.state.cedula}/>
             </FormGroup>
             <FormGroup>
-              <FormLabel>Ingrese el Apellido</FormLabel>
+              <FormLabel>Nombre:</FormLabel>
+              <FormControl type="text" name="nombre" placeholder="Ingrese el nombre." onChange={this.handleChange} value={this.state.nombre} required={true}></FormControl>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Apellido</FormLabel>
               <FormControl type="text" name="apellido" placeholder="Ingrese los apellidos." onChange={this.handleChange} value={this.state.apellido}/>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Telefono</FormLabel>
+              <FormControl type="text" name="telefono" placeholder="Ingrese el telefono." onChange={this.handleChange} value={this.state.telefono}/>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Telefono2</FormLabel>
+              <FormControl type="text" name="telefono2" placeholder="Ingrese el telefono2." onChange={this.handleChange} value={this.state.telefono2}/>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Direccion</FormLabel>
+              <FormControl type="text" name="direccion" placeholder="Ingrese la direccion." onChange={this.handleChange} value={this.state.direccion}/>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Correo electronico</FormLabel>
+              <FormControl type="text" name="email" placeholder="Ingrese la direccion." onChange={this.handleChange} value={this.state.email}/>
             </FormGroup>
             <Button onClick={this.addRegistro}>Guardar</Button>
           </Form>
--- formularios_mysql/servidor/index.js
+++ formularios_mysql/servidor/index.js
@@ -1,10 +1,28 @@
 const bodyParser = require('body-parser');
+const { response } = require('express');
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 const app = express();
 
 //Parse application/json
 app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
+
+/* app.use((req, res) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+}); */
 
 const conn = mysql.createConnection({
     host: "localhost",
@@ -27,6 +45,23 @@ app.listen(3001, () => {
     console.log("Corriendo en el puerto 3001");
 });
 
+//Agregar un registro.
 app.post("/api/create", (req, res) => {
-    
+    let data = {
+        id_cliente: 1,
+        cedula: req.body.cedula,
+        nombre: req.body.nombre, 
+        apellido: req.body.apellido,
+        telefono1: req.body.telefono,
+        telefono2: req.body.telefono2,
+        direccion: req.body.direccion,
+        email: req.body.email,
+    };
+    //let sql = "INSERT INTO cliente (id_cliente, cedula, nombre, apellido, telefono1, telefono2, direccion, email) VALUES " +
+    //"()";
+    let sql = "INSERT INTO cliente SET ?";
+    let query = conn.query(sql, data, (err, result) => {
+        if (err) throw err;
+        res.send(JSON.stringify({status: 200, error: null, response: "Nuevo regisstro anadido satisfactoriamente"}));
+    });
 });
