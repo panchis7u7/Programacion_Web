274d6ea30b3b3745ebe1ae67061488bf5c71dffa
Panchis7u7 <smadrigal.rod@gmail.com>
2021-01-08 19:02:46 -0600

    Formularios en Apache Cassandra.

---
+++ rCassandra/cliente/src/Components/NavBar.js
@@ -0,0 +1,9 @@
+import '../CSS/NavBar.css';
+
+export default function NavBar() {
+  return (
+    <div className="App">
+      Hola
+    </div>
+  );
+}
---
+++ rCassandra/cliente/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import NavBar from './Components/NavBar';
+import reportWebVitals from './Components/reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NavBar />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
