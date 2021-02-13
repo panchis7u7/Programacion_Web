0d4a5042653e49331216d971a95cf7c280369ba5
Panchis7u7 <smadrigal.rod@gmail.com>
2021-02-12 19:02:40 -0600

    Material table in react typescript.

--- C_Backend_Pruebas/cliente/src/App.tsx
+++ C_Backend_Pruebas/cliente/src/App.tsx
@@ -1,29 +1,61 @@
 import React from 'react';
+import MaterialTable, { Column } from 'material-table';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const App:React.FC = () => {
+  useEffect(() => {
+    fetchAlumnos();
+  }, []);
+
+  const columnas: Column<object>[] = [
+    {
+      title: 'ID',
+      field: 'id_alumno',
+      type: 'numeric'
+    },
+    {
+      title: 'Matricula',
+      field: 'matricula',
+      type: 'string'
+    },
+    {
+      title: 'Nombre',
+      field: 'nombre',
+      type: 'string'
+    },
+    {
+      title: 'Apellidos',
+      field: 'apellidos',
+      type: 'string'
+    },
+    {
+      title: 'Carrera',
+      field: 'carrera',
+      type: 'string'
+    }
+];
 
 const fetchAlumnos = () => {
   let headers = new Headers();
   headers.append("Content-Type", "application/json");
-  fetch("http://localhost:3490/prueba", {
+  fetch("http://localhost:3490/alumnos", {
     method: "GET",
     headers: headers,
   })
     .then((respuesta) => respuesta.json())
     .then((resultado) => {
       console.log("resultado: ", resultado);
+      setData(resultado);
   })
   .catch((error) => console.log("error: ", error));
 };
 
-const App:React.FC = () => {
-  useEffect(() => {
-    fetchAlumnos();
-  });
-
+  const [data, setData] = useState<object[]>([]);
   return (
     <div>
-      Hola
+      <MaterialTable columns={columnas} data={data}>
+      </MaterialTable>
     </div>
   );
 }
